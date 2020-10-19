247e33b553866bed0cab13015ca7640622009e7f
miriamela <grossi.miriam@gmail.com>
2020-10-19 14:11:03 +0200

    redoing

--- part3/phonebook/frontend/src/services/Persons.js
+++ part3/phonebook/frontend/src/services/Persons.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "https://secret-crag-54881.herokuapp.com/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
