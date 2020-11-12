82df6919688bef461bf15b68ce4c78d21ec49a4c
miriamela <grossi.miriam@gmail.com>
2020-11-12 13:26:44 +0100

    correcting tests

--- part4/bloglist/backend/tests/blog_api.test.js
+++ part4/bloglist/backend/tests/blog_api.test.js
@@ -2,7 +2,68 @@ const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
-// const Blog = require("../models/blog");
+const Blog = require("../models/blog");
+const initialBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url:
+      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url:
+      "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0,
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url:
+      "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0,
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  let initialArray = initialBlogs.map((blog) => new Blog(blog));
+  const blogs = await initialArray.map((blog) => blog.save());
+  await Promise.all(blogs);
+});
 
 test("blogs are returned as json", async () => {
   await api
@@ -11,12 +72,12 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
-test("there are 11 notes and they are json", async () => {
+test("return all blogs", async () => {
   const response = await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
-  expect(response.body).toHaveLength(11);
+  expect(response.body).toHaveLength(initialBlogs.length);
 });
 
 test("blogs have id property", async () => {
@@ -24,7 +85,29 @@ test("blogs have id property", async () => {
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
-  expect(response.body[0].id).toBeDefined();
+  const blogs = response.body;
+  for (let i = 0; i < blogs.length; i++) {
+    expect(blogs[i].id).toBeDefined();
+  }
+});
+
+test("a new blog post can be added", async () => {
+  const newBlog = {
+    title: "Avoiding Memory Leaks With CanJS",
+    author: "Sebastian",
+    url:
+      "http://sporto.github.io/blog/2014/03/20/avoiding-memory-leaks-with-canjs/",
+    like: "7",
+  };
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+  const response = await api.get("/api/blogs");
+  const content = response.body.map((x) => x.title);
+  expect(response.body).toHaveLength(initialBlogs.length + 1);
+  expect(content).toContain("Avoiding Memory Leaks With CanJS");
 });
 afterAll(() => {
   mongoose.connection.close();
