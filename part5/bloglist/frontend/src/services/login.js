93649085b78dbe0e7863796c7622705242e125d2
miriamela <grossi.miriam@gmail.com>
2020-12-12 14:39:17 +0100

    cleaned

--- part5/bloglist/frontend/src/services/login.js
+++ part5/bloglist/frontend/src/services/login.js
@@ -6,5 +6,4 @@ const login = async (credentials) => {
   const response = await axios.post(baseUrl, credentials);
   return response.data;
 };
-// eslint-disable-next-line import/no-anonymous-default-export
 export default { login };
