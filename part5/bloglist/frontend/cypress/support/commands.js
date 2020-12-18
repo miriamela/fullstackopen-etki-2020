8f8d98f2763b195af7562a7b483e283428779232
miriamela <grossi.miriam@gmail.com>
2020-12-18 14:29:40 +0100

    ereased comments

--- part5/bloglist/frontend/cypress/support/commands.js
+++ part5/bloglist/frontend/cypress/support/commands.js
@@ -21,38 +21,3 @@ Cypress.Commands.add("createBlog", ({ title, author, url, likes }) => {
   });
   cy.visit("http://localhost:3000");
 });
-
-// Cypress.Commands.add("sorted", ({ array }) => {
-//   for (i = 0; i < array.length; i++) {
-//     if (a[i] < a[i - 1]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// });
-
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
