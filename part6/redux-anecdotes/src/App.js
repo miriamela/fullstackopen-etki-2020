d9ce1c65f2396ef8ce5cc0f0fd208371105dc30e
miriamela <grossi.miriam@gmail.com>
2021-01-07 14:29:06 +0100

    added server and fetching data from server

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Notification from "./components/Notification";
+import { initializingAnecdotes } from "./reducers/anecdoteReducer";
+import { useDispatch } from "react-redux";
+import anecdotesService from "./services/anecdotes";
 import Filter from "./components/Filter";
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdotesService
+      .getAll()
+      .then((data) => dispatch(initializingAnecdotes(data)));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <h2>Anecdotes</h2>
