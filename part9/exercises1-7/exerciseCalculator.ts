187a566ebdaf2382ce6c98bec93b50ca4e48271f
miriamela <grossi.miriam@gmail.com>
2021-01-28 19:41:33 +0100

    setting up Patienter app

--- part9/exercises1-7/exerciseCalculator.ts
+++ part9/exercises1-7/exerciseCalculator.ts
@@ -1,13 +1,13 @@
 // EXERCISE 9.2
 
-interface Object{
-    numberOfDays: number,
-    numberOfTrainedDays: number,
+export interface Object{
+    periodLength: number,
+    trainingDays: number,
     target: number,
-    averageTime: number,
-    reachedTarget: boolean,
+    average: number,
+    success: boolean,
     rating:number,
-    explanation:string 
+    ratingDescription:string 
 }
 
 const calculateRating = (average:number, target:number):number=>{
@@ -27,19 +27,20 @@ const calculateRating = (average:number, target:number):number=>{
 const ratingSentence=(rating:number):string=>{
     console.log(rating)
 
-    if(rating ===1){
-        return "Nice try kid"
-    }
-    if(rating ===2){
-        return "Not too bad but you can do better, and you know it";
-    }
+    
     if(rating ===3){
         return "Well done you!"
+    }
+    else if(rating ===2){
+        return "Not too bad but you can do better, and you know it";
+    }
+    else{
+        return "Nice try kid" 
     }
 }
 
 
-const calculateExercise =(days: Array<number>, target:number):Object =>{
+export const calculateExercise =(days: Array<number>, target:number):Object =>{
     const numberOfDays= days.length;
     const numberOfTrainedDays= days.filter(each=> each !==0).length
     const average= days.reduce((total, amount)=> total+amount)/days.length;
@@ -63,36 +64,36 @@ return{
 
 // EXERCISE 9.3
 
-interface ValuesNeeded{
-    days: Array<number>,
-    target: number
+// interface ValuesNeeded{
+//     days: Array<number>,
+//     target: number | undefined,
     
-}
+// }
 
-const argvArgument =(args:Array<string>):ValuesNeeded=>{
+// const argvArgument =(args:Array<string>):ValuesNeeded=>{
     
-    if(args.length < 4){
-        throw new Error ("not enough arguments")
-    }
-    const numbers= args.slice(2).map(each=> Number(each))
-// first version the checked was performed with typeof each === "number", the issue is: when you change to
-// number whatever you have inside the array numbers, if there is s string it will output that element as NaN and
-// NaN, when goes through the check of typeof, is red as a Number... one of the random things of JS...
-    const allNumbers = numbers.every(each => !isNaN(each))
-    if(allNumbers){
-        const target =numbers.shift()
-        return{
-            days: numbers,
-            target: target,
-        }
-    }else{
-        throw new Error ("all data provided should be numbers")
-    }
-}
+//     if(args.length < 4){
+//         throw new Error ("not enough arguments")
+//     }
+//     const numbers= args.slice(2).map(each=> Number(each))
+// // first version the checked was performed with typeof each === "number", the issue is: when you change to
+// // number whatever you have inside the array numbers, if there is s string it will output that element as NaN and
+// // NaN, when goes through the check of typeof, is red as a Number... one of the random things of JS...
+//     const allNumbers = numbers.every(each => !isNaN(each))
+//     if(allNumbers){
+//         const target =numbers.shift()
+//         return{
+//             days: numbers,
+//             target: target,
+//         }
+//     }else{
+//         throw new Error ("all data provided should be numbers")
+//     }
+// }
 
-try{
-    const {days, target}= argvArgument(process.argv);
-    console.log(calculateExercise(days, target))
-}catch(e){
-    console.log(`Something went wrong: ${e.message}`)
-}
+// try{
+//     const {days, target}= argvArgument(process.argv);
+//     console.log(calculateExercise(days, target))
+// }catch(e){
+//     console.log(`Something went wrong: ${e.message}`)
+// }
--- part9/exercises1-7/index.ts
+++ part9/exercises1-7/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {calculateBmi} from "./bmiCalculator";
+import {calculateExercise} from "./exerciseCalculator";
 
 const app=express();
 
@@ -7,11 +8,7 @@ app.get("/hello", (_req, res)=>{
     res.send("Hello Full Stack")
 })
 
-const PORT =3003;
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on ${PORT}`)
-})
 
 app.get("/bmi", (req, res)=>{
     if (!req.query.height || !req.query.weight){
@@ -27,4 +24,27 @@ app.get("/bmi", (req, res)=>{
             bmi: result
         })
     }
+})
+
+app.use(express.json())
+
+app.post("/exercises", (req, res)=>{
+    const body= req.body;
+    if(!body.daily_exercises || !body.target){
+        res.json({
+            error: "parameters are missing"
+        })
+    }
+    if(!Array.isArray(body.daily_exercises) || isNaN(body.target)){
+        res.json({
+            error: "malformatted parameters"
+        })
+    }
+    res.json(calculateExercise(body.daily_exercises, body.target));
+
+})
+const PORT =3003;
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on ${PORT}`)
 })
